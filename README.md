06b61baffbe615ea6ae8dbadbc4845c2522851f4
Redi Ahmad <redi.ngoding@gmail.com>
2023-04-26 21:18:31 +0700

    add: target result short description

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # About this repo
 
 The goal is to recreate the website in [Next.js tutorial](https://nextjs.org/learn/basics/create-nextjs-app)
+
+The final result must be identical or even better than this [Next.js Sample Website](https://next-learn-starter.vercel.app/).
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import Head from 'next/head';
+import Layout from '@/components/Layout';
 
 export default function Home() {
-  return <h1 className={`${inter.className}`}>Hello World</h1>;
+  return (
+    <Layout home>
+      <Head>
+        <title>Reday Blogs</title>
+      </Head>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        inventore consequuntur vel, autem saepe quis soluta tempora? Maxime
+        vitae, officiis reprehenderit nisi ut perferendis odio hic voluptate
+        repellendus natus dolorum quia consectetur rem, nesciunt incidunt
+        magnam. Ad eos libero ullam temporibus itaque quis consequuntur ut! Vel
+        facere officiis autem corporis!
+      </p>
+    </Layout>
+  );
 }
