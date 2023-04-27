73b9ef5c1880aba56676a5a9cebefe5c4e48b95f
Redi Ahmad <redi.ngoding@gmail.com>
2023-04-27 20:13:56 +0700

    feat: show blog list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import Layout from '@/components/Layout';
+import getSortedPost, { PostsData } from '../lib/posts.utils';
 
-export default function Home() {
+export async function getStaticProps() {
+  const postMetadata: PostsData[] = getSortedPost();
+  return {
+    props: { postMetadata },
+  };
+}
+
+export default function Home({ postMetadata }) {
   const ownerDescription = `Hello, I'm Redi Ahmad, a Computer System student who wants to be a software engineer.`;
+  console.log(postMetadata);
   return (
     <Layout home>
       <Head>
         <title>Reday Blogs</title>
       </Head>
       <p>{ownerDescription}</p>
+      <section>
+        <h2>Blogs</h2>
+        <ul>
+          {postMetadata.map(({ id, date, title }: PostsData) => (
+            <li key={id}>
+              <h2>{title}</h2>
+              <h3>{id}</h3>
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
