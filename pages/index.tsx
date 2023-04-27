9a9d0abce9601b2071d4220caee92b74d13e087d
Redi Ahmad <redi.ngoding@gmail.com>
2023-04-27 14:48:19 +0700

    hardcode owner description

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,13 @@ import Head from 'next/head';
 import Layout from '@/components/Layout';
 
 export default function Home() {
+  const ownerDescription = `Hello, I'm Redi Ahmad, a Computer System student who wants to be a software engineer.`;
   return (
     <Layout home>
       <Head>
         <title>Reday Blogs</title>
       </Head>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-        inventore consequuntur vel, autem saepe quis soluta tempora? Maxime
-        vitae, officiis reprehenderit nisi ut perferendis odio hic voluptate
-        repellendus natus dolorum quia consectetur rem, nesciunt incidunt
-        magnam. Ad eos libero ullam temporibus itaque quis consequuntur ut! Vel
-        facere officiis autem corporis!
-      </p>
+      <p>{ownerDescription}</p>
     </Layout>
   );
 }
